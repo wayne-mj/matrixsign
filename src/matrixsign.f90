e3598fca4570e1fcb139b56aeaeae925e365e74c
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2026-05-14 21:20:30 +1000

    Added make_r function

--- src/matrixsign.f90
+++ src/matrixsign.f90
@@ -2,9 +2,35 @@ module matrixsign
   implicit none
   private
 
-  public :: say_hello
+  public :: make_r
 contains
-  subroutine say_hello
-    print *, "Hello, matrixsign!"
-  end subroutine say_hello
+  ! subroutine say_hello
+  !   print *, "Hello, matrixsign!"
+  ! end subroutine say_hello
+
+  function make_r(height,width) result(letter_str)
+    integer, intent(in)                   :: height, width
+    character(len=(height)*(width+1))   :: letter_str
+    integer                               :: row,col, pos
+
+    pos = 1
+    letter_str = ""
+    do row = 0, height-1
+      do col = 0, width-1
+        if ((col .eq. 1) .or. & 
+            (col .eq. 2 .and. (row .eq. 0 .or. row .eq. 2 .or. row .eq. 3)) .or. &
+            (col .eq. 3 .and. (row .eq. 1 .or. row .eq. 4 .or. row .eq. 5)) ) then
+          ! letter_str = letter_str // "*"
+          letter_str(pos:pos) = "*"
+        else
+          ! letter_str = letter_str // " "
+          letter_str(pos:pos) = "."
+        end if
+        pos = pos + 1
+      end do
+      ! letter_str = letter_str // new_line('a')
+      letter_str(pos:pos) = new_line('a')
+      pos = pos + 1
+    end do
+  end function
 end module matrixsign
