fca53810a45aa5f2023a93b38cbac60d47ce675b
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2026-05-15 12:18:44 +1000

    Added marquee function

--- app/main.f90
+++ app/main.f90
@@ -2,30 +2,35 @@ program main
   use matrixsign
   implicit none
   integer, parameter   :: height=6, width=5
-  character(height*(width+1)) :: letter
+  character(height*(4*width+1)) :: letter
+  character(len=4)  ::  word
 
-  letter = make_r(height, width)
-  ! print *, len(letter)
-  write (*,'(A)') letter
+  word = "BLUE"
+  letter = make_marquee(word, height, width)
+
+  write (*, '(A)') letter
+  ! letter = make_r(height, width)
+  ! ! print *, len(letter)
+  ! write (*,'(A)') letter
   
-  letter = make_e(height, width)
-  ! print *, len(letter)
-  write (*,'(A)') letter
+  ! letter = make_e(height, width)
+  ! ! print *, len(letter)
+  ! write (*,'(A)') letter
 
-  letter = make_d(height, width)
-  ! print *, len(letter)
-  write (*,'(A)') letter
+  ! letter = make_d(height, width)
+  ! ! print *, len(letter)
+  ! write (*,'(A)') letter
 
-  letter = make_b(height, width)
-  ! print *, len(letter)
-  write (*,'(A)') letter
+  ! letter = make_b(height, width)
+  ! ! print *, len(letter)
+  ! write (*,'(A)') letter
 
-  letter = make_l(height, width)
-  ! print *, len(letter)
-  write (*,'(A)') letter
+  ! letter = make_l(height, width)
+  ! ! print *, len(letter)
+  ! write (*,'(A)') letter
 
-  letter = make_u(height, width)
-  ! print *, len(letter)
-  write (*,'(A)') letter
+  ! letter = make_u(height, width)
+  ! ! print *, len(letter)
+  ! write (*,'(A)') letter
 end program main
 
